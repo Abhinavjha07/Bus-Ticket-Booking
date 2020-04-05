fa30eced90037e0c6f0f751172408f38b7fd0c4b
Abhinav Jha <31755654+Abhinavjha07@users.noreply.github.com>
2020-04-05 14:35:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const users = require('./controllers/users'); 
 const ticket = require('./controllers/tickets');
-const bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -11,10 +10,8 @@ app.use(bodyParser.json());
 app.use('/users', users);
 app.use('/tickets', ticket);
 
-// let hash = bcrypt.hashSync('hyperverge', 10);
-// console.log(hash);
 mongoose.connect(
-    process.env.DB_CONNECT || 'mongodb://localhost:27017/bus_reservation',
+    process.env.DB_CONNECT ,
     { useNewUrlParser: true }
 )
 mongoose.connection
@@ -23,7 +20,7 @@ mongoose.connection
         console.warn('Error in connecting to database', error);
     });
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT , ()=> {
 	console.log('Server started a port: 3000');
 });
 
