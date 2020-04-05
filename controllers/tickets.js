06786f967dd16752ebad0c4506e1ed5785071f8d
Abhinavjha07 <jhaabhinav1998@gmail.com>
2020-04-05 22:04:13 +0530

    fixed minor errors

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -191,7 +191,7 @@ router.get('/user_details/:seat_number', (req, res) => {
 })
 
 
-router.get('/admin', (req, res) => {
+router.post('/reset', (req, res) => {
 	if(!("username" in req.body) || !("password" in req.body)) {
 		res.status(400).json({message : "Username and Password are required to access it!"});
 	}
